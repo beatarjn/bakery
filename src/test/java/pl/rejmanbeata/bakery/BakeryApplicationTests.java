60ba6a2e29e472d4d39b839752b0e460f235907e
beatarjn <beatarejman89@gmail.com>
2024-03-11 17:16:44 +0100

    Bakery - initial gradle setup

---
+++ src/test/java/pl/rejmanbeata/bakery/BakeryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rejmanbeata.bakery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakeryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
