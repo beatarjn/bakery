36c07a942bd4c2fce8fdabf9d6b8f972e6312d01
beatarjn <beatarejman89@gmail.com>
2024-04-07 21:06:45 +0200

    add OrderEntity class

---
+++ src/main/java/pl/rejmanbeata/bakery/database/OrderEntity.java
@@ -0,0 +1,29 @@
+package pl.rejmanbeata.bakery.database;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "orders")
+public class OrderEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private ProductEntity product;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+
+
+//    Here, an Order can have a many-to-one relationship with Product, meaning many orders
+//    can be associated with one product. The @JoinColumn annotation specifies the foreign
+//    key column (product_id) in the orders table that maps to the primary key (id) in the
+//    products table.
+}
