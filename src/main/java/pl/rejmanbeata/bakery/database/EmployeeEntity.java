543fb91571d620fd4cecdbe100bc329e40cbbf61
beatarjn <beatarejman89@gmail.com>
2024-04-07 21:06:16 +0200

    add EmployeeEntity class

---
+++ src/main/java/pl/rejmanbeata/bakery/database/EmployeeEntity.java
@@ -0,0 +1,34 @@
+package pl.rejmanbeata.bakery.database;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "employees")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @ManyToOne
+    @JoinColumn(name = "address_id")
+    private AddressEntity address;
+
+    @Column(name = "role")
+    private String role;
+
+}
