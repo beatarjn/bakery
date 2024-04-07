6cdb585fb35587201a7fb3063d24458680388879
beatarjn <beatarejman89@gmail.com>
2024-04-07 21:07:20 +0200

    add ClientEntity class

---
+++ src/main/java/pl/rejmanbeata/bakery/database/ClientEntity.java
@@ -0,0 +1,41 @@
+package pl.rejmanbeata.bakery.database;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "clients")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ClientEntity {
+
+    @Id
+    @Column
+    private Long id;
+    @Column
+    private String name;
+    @Column
+    private String lastName;
+
+    @ManyToOne
+    @JoinColumn(name = "address_id")
+    private AddressEntity address;
+
+    @OneToOne
+    @JoinColumn(name = "order_id")
+    private OrderEntity order;
+
+//    Client can have a many-to-one relationship with Address, meaning many clients can have the same address.
+//    The @JoinColumn annotation specifies the foreign key column (address_id) in the clients table that maps
+//    to the primary key (id) in the address table.
+
+//     Client can have a one-to-one relationship with Order, meaning each client can have one order
+//     associated with them. The @JoinColumn annotation specifies the foreign key column (order_id)
+//     in the clients table that maps to the primary key (id) in the orders table.
+
+}
