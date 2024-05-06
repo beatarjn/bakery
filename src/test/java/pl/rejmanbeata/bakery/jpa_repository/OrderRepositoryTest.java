84b7e3d1b88a0bfcf5aef4c2b4e336bb57f16692
beatarjn <beatarejman89@gmail.com>
2024-05-06 20:04:24 +0200

    replace @SpringBootTest with @DataJpaTest for repository testing

--- src/test/java/pl/rejmanbeata/bakery/jpa_repository/OrderRepositoryTest.java
+++ src/test/java/pl/rejmanbeata/bakery/jpa_repository/OrderRepositoryTest.java
@@ -2,7 +2,7 @@ package pl.rejmanbeata.bakery.jpa_repository;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.test.context.ActiveProfiles;
 import pl.rejmanbeata.bakery.database.OrderEntity;
@@ -14,7 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ActiveProfiles("test")
-@SpringBootTest
+@DataJpaTest
 class OrderRepositoryTest {
 
     @Autowired
