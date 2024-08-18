2981c98cbc22d7ba88f28abb7d4695e198d1138d
beatarjn <beatarejman89@gmail.com>
2024-08-18 18:49:17 +0200

    Add Employee controller with tests

---
+++ src/main/java/pl/rejmanbeata/bakery/controller/EmployeeController.java
@@ -0,0 +1,68 @@
+package pl.rejmanbeata.bakery.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.rejmanbeata.bakery.model.employee.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+    private final List<Employee> employeeList = new ArrayList<>();
+
+    @GetMapping("/{name}")
+    public ResponseEntity<Employee> getEmployeeByName(@PathVariable String name) {
+        Optional<Employee> employee = employeeList.stream()
+                .filter(emp -> emp.getName().equalsIgnoreCase(name))
+                .findFirst();
+
+        return employee
+                .map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
+        employeeList.add(employee);
+        return new ResponseEntity<>(employee, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{name}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable String name, @RequestBody Employee updatedEmployee) {
+        Optional<Employee> existingEmployee = employeeList.stream()
+                .filter(emp -> emp.getName().equalsIgnoreCase(name))
+                .findFirst();
+
+        if (existingEmployee.isPresent()) {
+            Employee emp = existingEmployee.get();
+            emp.setLastName(updatedEmployee.getLastName());
+            emp.setRole(updatedEmployee.getRole());
+            return new ResponseEntity<>(emp, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{name}")
+    public ResponseEntity<Void> deleteEmployee(@PathVariable String name) {
+        Optional<Employee> existingEmployee = employeeList.stream()
+                .filter(emp -> emp.getName().equalsIgnoreCase(name))
+                .findFirst();
+
+        if (existingEmployee.isPresent()) {
+            employeeList.remove(existingEmployee.get());
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        return new ResponseEntity<>(employeeList, HttpStatus.OK);
+    }
+}
