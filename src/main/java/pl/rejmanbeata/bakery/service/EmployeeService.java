dea52feec67635ab50dd3f2ad18af82deff8a213
beatarjn <beatarejman89@gmail.com>
2024-09-03 20:03:13 +0200

    correct EmployeeService

--- src/main/java/pl/rejmanbeata/bakery/service/EmployeeService.java
+++ src/main/java/pl/rejmanbeata/bakery/service/EmployeeService.java
@@ -4,29 +4,48 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.rejmanbeata.bakery.database.EmployeeEntity;
 import pl.rejmanbeata.bakery.jpa_repository.EmployeeRepository;
+import pl.rejmanbeata.bakery.mapper.EmployeeMapper;
+import pl.rejmanbeata.bakery.model.employee.Employee;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class EmployeeService {
-
     private final EmployeeRepository employeeRepository;
+    private final EmployeeMapper employeeMapper;
 
-    public EmployeeEntity save(EmployeeEntity address) {
-        return employeeRepository.save(address);
+    public Employee save(EmployeeEntity employeeEntity) {
+        EmployeeEntity savedEmployee = employeeRepository.save(employeeEntity);
+        return employeeMapper.employeeEntityToEmployee(savedEmployee);
     }
 
-    public List<EmployeeEntity> getAllEmployees() {
-        return employeeRepository.findAll();
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll()
+                .stream()
+                .map(employeeMapper::employeeEntityToEmployee)
+                .toList();
     }
 
-    public EmployeeEntity getEmployeeById(Long id) {
-        return employeeRepository.findById(id).orElse(null);
+    public Employee getEmployeeById(Long id) {
+        return employeeMapper.employeeEntityToEmployee(employeeRepository.findById(id).orElse(null));
     }
 
     public void deleteEmployeeById(Long id) {
         employeeRepository.deleteById(id);
     }
 
+    public Employee updateEmployee(Long employeeId, Employee employee) {
+        Optional<EmployeeEntity> existingEmployee = employeeRepository.findById(employeeId);
+        if (existingEmployee.isPresent()) {
+            EmployeeEntity employeeEntity = existingEmployee.get();
+            employeeEntity.setLastName(employee.getLastName());
+            employeeEntity.setName(employee.getName());
+            employeeEntity.setRole(employee.getRole());
+            EmployeeEntity savedEmployee = employeeRepository.save(employeeEntity);
+            return employeeMapper.employeeEntityToEmployee(savedEmployee);
+        }
+        return null;
+    }
 }
