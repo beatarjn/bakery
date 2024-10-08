dc706ad363ac00ed3b8bb294cc933666b0820345
beatarjn <beatarejman89@gmail.com>
2024-10-08 10:24:38 +0200

    extract url to properties + refactor

--- bakery_service/src/main/java/pl/rejmanbeata/bakery/controller/ExampleController.java
+++ bakery_service/src/main/java/pl/rejmanbeata/bakery/controller/ExampleController.java
@@ -1,10 +1,11 @@
 package pl.rejmanbeata.bakery.controller;
 
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 public class ExampleController {
@@ -12,8 +13,7 @@ public class ExampleController {
 
     @GetMapping("/hello")
     public ResponseEntity<String> getHello() {
-        return new ResponseEntity<>("Hello World!", HttpStatus.OK);
+        return new ResponseEntity<>("Hello World!", OK);
     }
-
 
 }
--- buns_store/src/main/java/pl/rejmanbeata/buns/ExampleClientController.java
+++ buns_store/src/main/java/pl/rejmanbeata/buns/ExampleClientController.java
@@ -3,6 +3,7 @@ package pl.rejmanbeata.buns;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,11 +13,16 @@ import java.io.IOException;
 public class ExampleClientController {
 
     private OkHttpClient client = new OkHttpClient();
+    @Value("${bakery.url}")
+    private String bakeryUrl;
+    @Value("${bakery.port}")
+    private String bakeryPort;
+    private static final String SEND_HELLO = "/hello";
 
     @GetMapping("/sendHello")
     public String runHello() throws IOException {
         Request request = new Request.Builder()
-                .url("http://localhost:8010/hello")
+                .url(bakeryUrl + bakeryPort + SEND_HELLO)
                 .build();
 
         try (Response response = client.newCall(request).execute()) {
